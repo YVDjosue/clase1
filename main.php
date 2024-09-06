a53396fef847d7a877fc2971b2dd3b440784e1d2
Josue Valdez <consultores.jg@gmail.com>
2024-09-06 18:16:51 -0500

    mi tarea

--- main.php
+++ main.php
@@ -1,4 +1,25 @@
 <?php
-require_once("functions.php")
+require_once("functions.php");
 
+//josue
+josue_evaluaBisiesto(2021);
+echo "<br>";
+echo josue_factorial(5);
+echo "<br>";
+josue_esMayorDeEdad(22);
+echo "<br>";
+josue_saludame("Josue");
+echo "<br>";
+echo josue_obtenerPromedio([8, 9, 10, 7, 8, 9, 10]);
+echo "<br><br><br><br>";
+
+//juacko
+
+
+
+//lalo
+
+
+
+//rafa
 ?>
